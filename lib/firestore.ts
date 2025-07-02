5829d759a00143e9146b1be65085301de7832b6c
BPChance <bradenpchance6@gmail.com>
2025-07-02 01:28:54 -0500

    added getPost function to firestore.ts for getting up to ten posts

--- lib/firestore.ts
+++ lib/firestore.ts
@@ -1,9 +1,19 @@
 // firestore functions
 
 import { db } from "@/firebaseConfig";
-import { addDoc, collection } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  DocumentSnapshot,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  startAfter,
+} from "firebase/firestore";
 
-type Post = {
+export type Post = {
+  id: string;
   caption: string;
   image: string;
   createdAt: Date;
@@ -16,6 +26,30 @@ async function addPost(post: Post) {
   await addDoc(posts, post);
 }
 
+async function getPost(startAfterDoc?: DocumentSnapshot) {
+  const postsRef = collection(db, "posts");
+
+  const q = startAfterDoc
+    ? query(
+        postsRef,
+        orderBy("createdAt", "desc"),
+        limit(10),
+        startAfter(startAfterDoc)
+      )
+    : query(postsRef, orderBy("createdAt", "desc"), limit(10));
+
+  const snapshot = await getDocs(q);
+  const posts = snapshot.docs.map((doc) => {
+    const data = doc.data() as Omit<Post, "id">;
+    return { id: doc.id, ...data };
+  });
+
+  const lastDoc = snapshot.docs[snapshot.docs.length - 1];
+
+  return { posts, lastDoc };
+}
+
 export default {
   addPost,
+  getPost,
 };
